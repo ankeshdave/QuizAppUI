7ef36e781808442bcad882fc7ca5f2b0eb89ffbf
Ankesh dave <smartyad@gmail.com>
2013-12-04 16:56:21 +0530

    Changes

--- QuizApp/QuizApp.UI/App_Start/BundleConfig.cs
+++ QuizApp/QuizApp.UI/App_Start/BundleConfig.cs
@@ -37,7 +37,7 @@ namespace QuizApp.UI
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css")); 
         }
     }
 }
