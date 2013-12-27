2bee0ae14a4a2da4a7eb15d3e4cfd6d7b63b4703
Ankesh Dave <smartyad@gmail.com>
2013-12-27 20:33:38 +0530

    Commit for Register.js

--- QuizApp/QuizApp.UI/Scripts/ViewScripts/Register.js
+++ QuizApp/QuizApp.UI/Scripts/ViewScripts/Register.js
@@ -4,7 +4,13 @@ $(document).ready(function () {
     /*Register User Form Start*/
     $('#validateRegisterForm').validationEngine('attach', {
          promptPosition : "topRight",
-         ajaxFormValidationMethod: 'post'
+         ajaxFormValidationMethod: 'post',
+        // Auto-hide prompt
+         autoHidePrompt: true,
+        // Delay before auto-hide
+         autoHideDelay: 3000,
+        // Fade out duration while hiding the validations
+         fadeDuration: 0.3,
     });
     $('#hidePromptRegisterForm').on('click', function () {
         $('#validateRegisterForm').validationEngine('hide');
