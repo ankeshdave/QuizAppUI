2934c1ee56979c9572f6b74404fef496e9c7fdb0
Akanksha Gaur <akankshagaur88@gmail.com>
2014-01-02 00:01:25 +0530

    added author area

---
+++ QuizApp/QuizApp.UI/Areas/Author/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizApp.UI.Areas.Author.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Author/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- QuizApp/QuizApp.UI/Controllers/CoursesController.cs
+++ QuizApp/QuizApp.UI/Controllers/CoursesController.cs
@@ -9,13 +9,12 @@ namespace QuizApp.UI.Controllers
 {
     public class CoursesController : Controller
     {
-        //
-        // GET: /Courses/
-        public ActionResult Index()
+        CourseInfoModel courseInfoModel = new CourseInfoModel();
+
+        public CoursesController()
         {
-            var courseInfoModel = new CourseInfoModel();
-            courseInfoModel.CourseInfos.Add(new CourseInfo(){CourseId = 1, Name="Course1",Description = "CourseDescription",CourseTags = new List<string>(){"PHP","RUBY"}});
-            courseInfoModel.CourseInfos.Add(new CourseInfo() {CourseId = 2,Name = "Course2", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
+            courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 1, Name = "Course1", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
+            courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 2, Name = "Course2", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 3, Name = "Course3", Description = "CourseDescription", CourseTags = new List<string>() { "JAVASCRIPT", "RUBY" } });
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 4, Name = "Course4", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "JAVASCRIPT" } });
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 5, Name = "Course5", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "JAVASCRIPT", "RUBY" } });
@@ -23,12 +22,19 @@ namespace QuizApp.UI.Controllers
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 7, Name = "Course7", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 8, Name = "Course8", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
             courseInfoModel.CourseInfos.Add(new CourseInfo() { CourseId = 9, Name = "Course9", Description = "CourseDescription", CourseTags = new List<string>() { "PHP", "RUBY" } });
+            
+        }
+        //
+        // GET: /Courses/
+        public ActionResult Index()
+        {
+            
             return View(courseInfoModel);
         }
         
         public ActionResult DisplayCourse(int courseId)
         {
-            return View();
+            return View(courseInfoModel.CourseInfos.FirstOrDefault(t=> t.CourseId == courseId));
         }
 
     }
