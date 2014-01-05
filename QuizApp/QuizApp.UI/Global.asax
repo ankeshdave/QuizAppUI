3f1e720667cc481d7ec8dc99df827311ab54579e
Ankesh Dave <smartyad@gmail.com>
2014-01-05 16:48:38 +0530

    Changes in the Design

--- QuizApp/QuizApp.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuizApp.UI.MvcApplication" Language="C#" %>
--- QuizApp/QuizApp.UI/Scripts/ViewScripts/ContactUsMap.js
+++
@@ -1,2 +0,0 @@
-﻿/// <reference path="~/Scripts/jquery-2.0.3.js" />
-/// <reference path="~/Scripts/jquery-ui-1.10.3.js" />
